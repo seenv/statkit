c2856d9dd5c53bdeab6b1f434d989d77f24caa4d
seenv <seena@uchicago.edu>
2026-05-19 08:52:40 -0500

    Resolving the conflicts

--- experiment/iperf.py
+++ experiment/iperf.py
@@ -7,64 +7,60 @@ from config import Config
 
 from utils import restart_gridftp, get_stream_id, start_tunnel
 from utils import stop_tunnel, delete_tunnel, record_ping, gridftp_config
-from utils import init_listener_env, init_initiator_env
+from utils import init_listener_env, init_initiator_env, gridftp_report
 from utils import start_statkit, stop_statkit, cleanup_iperf
 from utils import start_iperf_server, start_iperf_client
 from utils import base_start_iperf_server, base_start_iperf_client
 
+# UDP(default 1 Mbit/sec for UDP, unlimited for TCP) or TCP? xmit/recv the specified file
+# dont-fragment? zerocopy? TCP/SCTP no delay, disabling Nagle's Algorithm
 
 def iperf_main(cfg: Config) -> None:
     test_config = (
-        (block, duration, parallel, run)
+        (splice, block, duration, parallel, run)
+        for splice in cfg.splice
         for block in cfg.blocks
         for duration in cfg.time_frames
         for parallel in cfg.parallels
         for run in range(1, cfg.run_num + 1)
     )
-    tmp = None
-    total_runs = len(cfg.time_frames) * len(cfg.parallels) * cfg.run_num * len(cfg.blocks)
-    for idx, (block, duration, parallel, run) in enumerate(test_config, start=1):
+
+    last_block, last_splice = None, None
+    total_runs = len(cfg.splice) * len(cfg.time_frames) * len(cfg.parallels) * cfg.run_num * len(cfg.blocks)
+    for idx, (splice, block, duration, parallel, run) in enumerate(test_config, start=1):
         logging.info(
-            "--------------- Test %d / %d : blocksize %s / duration: %s / parallel %s / run %s ---------------",
+            "--------------- Test %d / %d : splice %s / blocksize %s / duration: %s / parallel %s / run %s ---------------",
             idx, total_runs, block, duration, parallel, run)
 
-        if block != tmp:
-            gridftp_config(cfg, block)
-            tmp = block
+        if block != last_block or splice != last_splice:
+            logging.info("Applying GridFTP configuration: splice: %s and blocksize: %sM", splice, block)
+            gridftp_config(cfg, block, splice)
+            last_block, last_splice = block, splice
 
-        tunnel_out_dir = f"{cfg.report_dir}/globus/{block}/{parallel}/{run}"
-        direct_out_dir = f"{cfg.report_dir}/direct/{block}/{parallel}/{run}"
-
+        tunnel_out_dir = f"{cfg.report_dir}/S{splice}/globus/{block}/{parallel}/{run}"
+        direct_out_dir = f"{cfg.report_dir}/S{splice}/direct/{block}/{parallel}/{run}"
+        #output_dir = f"{cfg.report_dir}/S{splice}/direct/{block}/{parallel}/{run}"
+        
         # launch the baseline test
         if cfg.baseline:
             try:
+                logging.info("GFTP: Recording the Gridftp configuration")
+                gridftp_report(cfg, direct_out_dir)
+                time.sleep(cfg.sleep)
                 # launch statkit
                 logging.info("BASELINE: Starting the statkit monitoring on the hosts")
                 start_statkit(cfg, duration, parallel, run, direct_out_dir)
                 time.sleep(cfg.sleep)
                 
-<<<<<<< Updated upstream
                 logging.info("BASELINE: Starting iperf server")
                 base_start_iperf_server(cfg, cfg.hosts.ap.get("listener"), cfg.ap_port, direct_out_dir)
-=======
-                logging.info("BASE: Starting iperf server")
-                #base_start_iperf_server(cfg, cfg.hosts.ap.get("listener"), cfg.ap_port, direct_out_dir)
-                base_start_iperf_server(cfg, cfg.hosts.ep.get("listener"), cfg.direct_port, temp_file, "iperf_base", direct_out_dir)
->>>>>>> Stashed changes
                 time.sleep(cfg.sleep)           # it takes more for them to initiates! TODO: find a better way
                 
                 # run iperf client
                 logging.info("BASELINE: Starting iperf client")
                 base_start_iperf_client(
-<<<<<<< Updated upstream
                     cfg, cfg.hosts.ap.get("initiator"), cfg.listener_ap_ip, 
                     cfg.ap_port, duration, parallel, direct_out_dir)
-=======
-                    #cfg, cfg.hosts.ap.get("initiator"), cfg.listener_ap_ip, 
-                    cfg, cfg.hosts.ep.get("initiator"), cfg.listener_ip,
-                    #cfg.ap_port, duration, parallel, direct_out_dir)
-                    cfg.direct_port, size, temp_file, "iperf_base", direct_out_dir)     #for port used ap to distinguish
->>>>>>> Stashed changes
                 
                 logging.info("BASELINE: Recording the RTT")
                 record_ping(cfg, cfg.hosts.ap.get("initiator"), cfg.listener_ap_ip, direct_out_dir)
@@ -87,6 +83,9 @@ def iperf_main(cfg: Config) -> None:
         time.sleep(cfg.sleep)
 
         try:
+            logging.info("GFTP: Recording the Gridftp configuration")
+            gridftp_report(cfg, tunnel_out_dir)
+            time.sleep(cfg.sleep)
             # launch statkit
             logging.info("IPERF: Starting the statkit monitoring on the hosts")
             start_statkit(cfg, duration, parallel, run, tunnel_out_dir)
@@ -102,13 +101,8 @@ def iperf_main(cfg: Config) -> None:
             contact_port = init_initiator_env(cfg, tunnel_id)
             #time.sleep(cfg.sleep)
             
-<<<<<<< Updated upstream
             logging.info("IPERF: Starting iperf server")
             start_iperf_server(cfg, cfg.hosts.ep.get("listener"), cfg.ep_port, tunnel_id, tunnel_out_dir)
-=======
-            logging.info("GST: Starting iperf server")
-            start_iperf_server(cfg, cfg.hosts.ep.get("listener"), cfg.tunnel_port, tunnel_id, temp_file, "iperf_gst", tunnel_out_dir)
->>>>>>> Stashed changes
             time.sleep(cfg.sleep)           # it takes more for them to initiates! TODO: find a better way
             
             # run iperf client
@@ -134,5 +128,5 @@ def iperf_main(cfg: Config) -> None:
             time.sleep(cfg.sleep)
             restart_gridftp(cfg)
             
-            # delete_tunnel(cfg, tunnel_id)
-            # time.sleep(cfg.sleep)
+            delete_tunnel(cfg, tunnel_id)
+            time.sleep(cfg.sleep)
